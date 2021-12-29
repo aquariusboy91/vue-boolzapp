f84de8d1babdf4fed8de9f0c21ebde86ba2ec574
Acquariusboy91 <luca.pellegrino91@yahoo.it>
2021-12-29 16:20:44 +0100

    dropdrown rev2

--- js/script.js
+++ js/script.js
@@ -84,16 +84,19 @@
               date: "20/03/2020 16:30:00",
               text: "Ciao come stai?",
               status: "sent",
+              drop_visible: false
             },
             {
               date: "20/03/2020 16:30:55",
               text: "Bene grazie! Stasera ci vediamo?",
               status: "received",
+              drop_visible: false
             },
             {
               date: "20/03/2020 16:35:00",
               text: "Mi piacerebbe ma devo andare a fare la spesa.",
               status: "sent",
+              drop_visible: false
             },
           ],
         },
@@ -107,16 +110,19 @@
               date: "28/03/2020 10:10:40",
               text: "La Marianna va in campagna",
               status: "received",
+              drop_visible: false
             },
             {
               date: "28/03/2020 10:20:10",
               text: "Sicuro di non aver sbagliato chat?",
               status: "sent",
+              drop_visible: false
             },
             {
               date: "28/03/2020 16:15:22",
               text: "Ah scusa!",
               status: "received",
+              drop_visible: false
             },
           ],
         },
@@ -129,11 +135,13 @@
               date: "10/01/2020 15:30:55",
               text: "Lo sai che ha aperto una nuova pizzeria?",
               status: "sent",
+              drop_visible: false
             },
             {
               date: "10/01/2020 15:50:00",
               text: "Si, ma preferirei andare al cinema",
               status: "received",
+              drop_visible: false
             },
           ],
         },
